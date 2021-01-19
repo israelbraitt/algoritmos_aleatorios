8060e9fca04e4a552f31db7f9bd105132695b3ca
israelbraitt <59903080+israelbraitt@users.noreply.github.com>
2021-01-19 01:18:21 -0300

    Criando soma de ímpares consecutivos.cpp

---
+++ soma de ímpares consecutivos.cpp
@@ -0,0 +1,33 @@
+/*
+Calcula e mostra a soma dos números ímpares entre o intervalo dos valores inteiros x e y.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y, soma = 0;
+    int min, max;
+   
+    cin >> x >> y;
+   
+    if(x < y){
+        min = x;
+        max = y;
+    }else{
+        max = x;
+        min = y;
+    }
+   
+    for(int i = (min + 1); i < max; ++i){
+        if(i % 2 == 1 || i % 2 == -1){
+            soma += i;
+        }
+    }
+   
+    cout << soma << endl;
+   
+    return 0;
+}
